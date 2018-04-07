833a61a80e821adf5f91e8cffea05eca54acfca1
cydolo <dolomac99@gmail.com>
2018-04-07 14:02:31 +0200

    Update main.js

--- Badges/main.js
+++ Badges/main.js
@@ -1,1 +1,34 @@
 var api = "https://clientapi.myteamspeak.com/";
+var post = {
+    methode: {
+        1: {a: "authentication"},
+        2: {b: "synchronization"},
+        3: {c: "user"}
+    }
+};
+class Badges {
+    static Add() {   
+    try {
+        const sender = new XMLHttpRequest();
+    } catch (error) {
+
+    }
+ }
+ static Remove() {
+     try {
+         const sender = new XMLHttpRequest();
+         sender.open("POST",api + post.methode[3].c,true);
+         sender.onreadystatechange = function(ok) {
+             if (sender.status == "200") {
+                 console.log("Alright, see what happen.." + sender.responseText);
+             }else {
+                 console.log("um.. nope" + sender.responseText);
+             }
+         }
+         //todo
+     } catch (error) {
+        console.log("something happend.." + error);
+     }
+ }
+
+}
